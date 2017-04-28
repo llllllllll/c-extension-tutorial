a4a11a1741cd00dcccb2f1245fe45c72dcea8fc0
Joe Jevnik <joe@quantopian.com>
2017-04-28 18:52:57 -0400

    DEV: fix .dir-locals for pyconfig.h issue

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
-((c-mode . ((eval . (setq flycheck-gcc-include-path
-                          '("../../../submodules/cpython/Include"))))))
+((c-mode . ((c-file-style . "stroustrup")
+            (eval . (setq flycheck-gcc-include-path
+                          '("../../../submodules/cpython/Include"
+                            "../../../submodules/cpython"))))))
