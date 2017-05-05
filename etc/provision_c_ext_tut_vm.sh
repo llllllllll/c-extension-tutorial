7bbb38da478070268280552a9265f8e67fcdd001
Joe Jevnik <joe@quantopian.com>
2017-05-05 18:40:01 -0400

    DEV: install ntpd on the vm so make doesn't freak out with the timestamps

--- etc/provision_c_ext_tut_vm.sh
+++ etc/provision_c_ext_tut_vm.sh
@@ -2,6 +2,7 @@ set -e
 echo "provisioning c_extension_tutorial VM"
 
 dnf builddep -y python3
-dnf install -y gdb
+dnf install -y gdb ntpd
+sudo ntpd
 
 echo "c_extension_tutorial VM provisioned successfully"
